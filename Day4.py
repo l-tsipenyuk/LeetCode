3487ff6809b01b435b8fd71b371c1188c46460e4
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-11 13:42:34 +0300

    day4

--- Day4.py
+++ Day4.py
@@ -2,26 +2,42 @@
 
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        # if prices.index(max(prices)) < prices.index(min(prices)):
-        #     return max(prices) - min(prices)
-        # else:
-        #     for i in prices:
-        for k in range(len(prices)):
-            buy = min(prices)
-            if prices.index(buy) == len(prices):
-                return 0
-            if prices.index(buy) == k:  
+        if not prices:
+            return 0  
+
+        buy = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < buy:
+                buy = price  
+            else:
+                max_profit = max(max_profit, price - buy)  
+                
+
+        return max_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+# prices = [7, 6, 4, 3, 1]
+# prices = [2, 4, 1]
+
+solution = Solution()
+result = solution.maxProfit(prices)
+print(result)
+
+
+
+
                   
 
 
 
 
-# prices = [7,1,5,3,6,4]
-prices = [7,6,4,3,1]
+prices = [7,1,5,3,6,4]
+# prices = [7,6,4,3,1]
+# prices = [2,4,1]
+
+
 solution = Solution()
 result = solution.maxProfit(prices)
 print(result)
