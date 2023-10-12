a84c9e505c049da3f871725ac89f46a941b3fc48
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-12 15:01:42 +0300

    day5

--- Day5.py
+++ Day5.py
@@ -1,30 +1,54 @@
-# jump game
+# # jump game
 
-class Solution(object):
-    def canJump(self, nums):
-        if len(nums) <= 1:
-            return True
+# class Solution(object):
+#     def canJump(self, nums):
+#         if len(nums) <= 1:
+#             return True
 
-        max_reach = nums[0]
+#         max_reach = nums[0]
 
-        for i in range(len(nums)):
-            if i > max_reach:
-                return False
+#         for i in range(len(nums)):
+#             if i > max_reach:
+#                 return False
 
-            max_reach = max(max_reach, i + nums[i])
+#             max_reach = max(max_reach, i + nums[i])
 
-            if max_reach >= len(nums) - 1:
-                return True
+#             if max_reach >= len(nums) - 1:
+#                 return True
 
-        return False
+#         return False
 
-# Test cases
-nums1 = [2, 3, 1, 1, 4]
-nums2 = [3, 2, 1, 0, 4]
+# # Test cases
+# nums1 = [2, 3, 1, 1, 4]
+# nums2 = [3, 2, 1, 0, 4]
 
-solution = Solution()
-result1 = solution.canJump(nums1)
-result2 = solution.canJump(nums2)
+# solution = Solution()
+# result1 = solution.canJump(nums1)
+# result2 = solution.canJump(nums2)
 
-print(result1) 
-print(result2)  
+# print(result1) 
+# print(result2)  
+
+
+
+# dataframe
+
+import pandas as pd
+
+def createDataframe(student_data):
+    df = student_data
+    res = pd.DataFrame(data = df, columns = ["student_id", "age"])
+    return res
+
+
+
+student_data = [
+  [1, 15],
+  [2, 11],
+  [3, 11],
+  [4, 20]
+]
+
+
+result = createDataframe(student_data)
+print(result)
