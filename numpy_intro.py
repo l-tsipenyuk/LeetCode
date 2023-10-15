3b40c639db6a0528bb9dc3996c7d103533cfe05b
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-15 13:47:41 +0300

    numpy intro

---
+++ numpy_intro.py
@@ -0,0 +1,11 @@
+import numpy as np
+#For 0-D arrays we don't need square brackets:
+arr_1 = np.array(3)
+# print(arr_1)
+
+
+# Examples of 2-D and 3-D arrays:
+arr_1 = np.array([[1,5,2],[34,2,7]])
+arr_2 = np.array([[[5,3,8],[6,1,9]],[[5,3,8],[6,1,9]]])
+
+print(arr_1, arr_2)
