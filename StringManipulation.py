2843b09591dd7a48a101a6324284504417fe6692
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-15 13:30:07 +0300

    string manipulation

---
+++ StringManipulation.py
@@ -0,0 +1,14 @@
+# string = "String Manipulation in Python"
+
+
+#String slicing: i.e. print elements from 1 to 10 with step 2 
+# print(string[1:10:2])
+
+# We can access the very last element by using negative index -1.
+# With negative indexing, enumeration starts with 1 this time, i.e. the very last element has index -1, the last but one -2, and so on...
+
+# We can reverse a string by using [::-1]. 
+# This indexation means start at the end of string, end at the start (index 0), and move with step -1 (backward).
+
+# print(string[::-1])
+
