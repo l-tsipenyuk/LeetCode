04cb83b78cf064799097cf40e0ce528ecb46d432
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-16 15:41:41 +0300

    pandas

--- pandas_intro.py
+++ pandas_intro.py
@@ -10,7 +10,10 @@ data = pd.read_csv('https://codefinity-content-media.s3.eu-west-1.amazonaws.com/
 
 
 # Extract data with even and odd indices
-even = data.iloc[lambda x: x.index % 2 == 0 ]
-odd = data.iloc[lambda x: x.index % 2 != 0 ]
+# even = data.iloc[lambda x: x.index % 2 == 0 ]
+# odd = data.iloc[lambda x: x.index % 2 != 0 ]
 
-print(even.head(5), odd.tail(5))
+# print(even.head(5), odd.tail(5))
+
+# Extract data from the first 50 rows and columns 1 and 4
+# data_extracted = data.iloc[:50, [1,4]]
